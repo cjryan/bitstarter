61b6ef0026beb6c5e4bc67e8f7a6063e0755198c
Chris Ryan <cjryan@email.arizona.edu>
2013-07-10 19:26:03 +0000

    Added the index.html file to be read by the fs module.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  res = fs.readFileSync(filename);
+  res = fs.readFileSync('index.html');
   response.send(res);
 });
 
